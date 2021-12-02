faf8a55f9775424e2fb2c4d4ae6464d6dba67632
Maryam1602 <95431829+Maryam1602@users.noreply.github.com>
2021-12-02 17:14:58 +0100

    Update code.java

--- code.java
+++ code.java
@@ -1,1 +1,28 @@
+package puissance4;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class projet {
+	public static int entierAleatoire(int a, int b){
+		//Retourne un entier alÃ©atoire entre a (inclus) et b (inclus)
+		return ThreadLocalRandom.current().nextInt(a, b + 1);	
+	}
+	
+	
+	public static void afficheTab2D(int[][]tab) {
+		for(int i = 0; i<tab.length; i++) {
+			for(int j = 0; j<tab[i].length; j++)
+				System.out.print(tab[i][j] + " ");
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	
+
+	public static void main(String[] args) {
+
+	}
+}
 
